0031b028008cba22690473012b5b04d8ac6dc3e9
AyeshaAkram1 <137284023+AyeshaAkram1@users.noreply.github.com>
2023-07-21 11:52:50 +0500

    React Redux Example

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={dispatch(()=>Increment())}>+</button>
+        <span>{state.count}</span>
+      <button onClick={dispatch(()=>Decrement())}>-</button>
     </div>
   );
 }
